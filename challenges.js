0ba06d4d2e62e86c3ac626e46b30095decda427f
bokkle <mitch.r_@hotmail.com>
2024-01-03 23:12:49 -0500

    constructors

--- challenges.js
+++ challenges.js
@@ -59,3 +59,96 @@ const mercedesTwo = new CarTwo('Mercedes', 95);
 bmwTwo.accelerateTwo();
 bmwTwo.accelerateTwo();
 bmwTwo.accelerateTwo();
+
+// CODING CHALLENGE #2
+/* Your tasks:
+1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+by 1.6)
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+converts it to km/h before storing the value, by multiplying the input by 1.6)
+4. Create a new car and experiment with the 'accelerate' and 'brake'
+methods, and with the getter and setter.
+Test data:
+§ Data car 1: 'Ford' going at 120 km/h
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(
+      `The ${this.make} is travelling at ${this.speed}kmh or ${Math.ceil(
+        this.speedUS
+      )}mph`
+    );
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(
+      `The ${this.make} is travelling at ${this.speed}kmh or ${Math.ceil(
+        this.speedUS
+      )}mph`
+    );
+  }
+
+  get speedUS() {
+    // console.log(this.speed);
+    return this.speed / 1.6;
+  }
+
+  set speedUS(mph) {
+    // console.log(mph);
+    return (this.speedUS = mph);
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.brake();
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+
+// INSTRUCTOR SOLUTION
+
+class CarClInstructor {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const fordInstructor = new CarClInstructor('Ford', 120);
+console.log(fordInstructor.speedUS);
+fordInstructor.accelerate();
+fordInstructor.accelerate();
+fordInstructor.brake();
+fordInstructor.speedUS = 50;
+console.log(fordInstructor);
--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 // called with the 'new' operator
 
 //constructor functions start with a capital letter: convention
+
+/*
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -13,9 +15,9 @@ const Person = function (firstName, birthYear) {
 
   // Never do this below, bad practice
   // never create a method inside constructor function
-  /*this.calcAge = function() {
-        console.log(2023 - this.birthYear)
-    }*/
+  //   this.calcAge = function() {
+  //         console.log(2023 - this.birthYear)
+  //     }
 };
 
 const mitch = new Person('Mitch', 1996);
@@ -68,5 +70,142 @@ Array.prototype.unique = function () {
 };
 console.log(arr.unique());
 
-const h1 = document.querySelector('h1')
-console.dir(x => x + 1)
+const h1 = document.querySelector('h1');
+console.dir((x) => x + 1);
+
+// class expression
+const personCl = class {};
+
+//class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // these methods, written outside of the constructor, are on the prototype of
+  //the objects, and not on the objects themselves
+  // aka methods will be added to the .prototype property
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+
+  get age() {
+    return 2024 - this.birthYear;
+  }
+  //set a property that already exists, use a _ (as a convention)
+  set fullName(nameValue) {
+    console.log(nameValue);
+    if (nameValue.includes(' ')) this._fullName = nameValue;
+    else alert(`${nameValue} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log('Hey there :):)');
+    console.log(this);
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+console.log(jessica.__proto__ === PersonCl.prototype); // true
+
+//can also do this instead of adding methods below constructor ^
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+jessica.greet();
+
+// important notes:
+// 1. Classes are NOT hoisted
+// 2. Classes are first class citizens (can pass into/return from funcs)
+// 3. Classes are executed in strict mode
+
+// GETTERS AND SETTERS
+// every obj in JS can have getter and setter properties
+// these are funcs that get and set a value
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'Mitch',
+  movements: [200, 150, 120, 2800],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+console.log(account.latest);
+
+// STATIC METHODS
+
+Person.hey = function () {
+  console.log('Hey there :)');
+  console.log(this);
+};
+Person.hey();
+
+PersonCl.hey();
+
+const PersonProto = {
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
+*/
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2024 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  //this.firstName/birthYear is already in Person, bad practice to repeat code
+  //   this.firstName = firstName;
+  //   this.birthYear = birthYear;
+  //instead, call Person and set properties using this syntax
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}.`);
+};
+
+const mike = new Student('Mike', 2000, 'Computer Science');
+console.log(mike);
+mike.introduce();
